bdc9fc2d9ef479ead2d96fb6cc4c33b4c3e6a1ef
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2021-03-01 19:59:25 -0500

    update

--- site.php
+++ site.php
@@ -4,7 +4,17 @@
 </head>
 <body>
 <?php
-echo"<h1>grwegrere</h1>";
+$a="WWWWWWWWewq";
+$b=22;
+echo"<h1>$a fdd  $b</h1>";
+$b="fewfewe";
+echo"<h1>$a fdd  $b</h1>";
+echo 332.33;
+echo false;
+echo strtolower($a);
+echo strlen($a);
+echo $a[0];
+echo $b[0];
 ?>
 </body>
 </html>
