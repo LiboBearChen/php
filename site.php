d690220d1f4cc6c2552e0ccb4ace591e76707117
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2021-03-02 17:16:31 -0500

    update

--- site.php
+++ site.php
@@ -9,12 +9,20 @@ $b=22;
 echo"<h1>$a fdd  $b</h1>";
 $b="fewfewe";
 echo"<h1>$a fdd  $b</h1>";
-echo 332.33;
-echo false;
-echo strtolower($a);
-echo strlen($a);
-echo $a[0];
-echo $b[0];
+$b[0]="S";
+echo $b;
+echo "Qwefew"[0];
+
+echo str_replace("S","222222222222222",$b);
+echo"    ";
+echo substr($b,3);
+echo 3*3.33333;
+echo abs(-3432);
 ?>
+<form action="site.php" method="get">
+Name:           <input type="text" name="name">
+<input type="submit">
+<?php  ?>
+</form>
 </body>
 </html>
