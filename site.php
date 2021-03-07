d6577d31d59255e1d2b3e9ddfbf7a4ce4246412a
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2021-03-07 17:43:35 -0500

    uppdate

--- site.php
+++ site.php
@@ -6,20 +6,37 @@
 
 <br>
 <form action="site.php" method="post">
-num1: <input type="text" name="num1" ><br>
-OP: <input type="textbox" name="op" ><br>
-num2: <input type="text" name="num2" ><br>
+num1: <input type="number" step="0.2" name="num1" ><br>
+OP: <input type="text" name="op" ><br>
+num2: <input type="number" name="num2" ><br>
+<input type="submit">
+</form>
+<br>
+<form action="site.php" method="post">
+Grade: <input type="text" name="grade" ><br>
 <input type="submit">
 </form>
 <?php  
-function q($ww){
-    echo "!!!!!!!$ww<br>";
+$num1=$_POST["num1"];
+$num2=$_POST["num2"];
+$op=$_POST["op"];
+if($op=="+"){
+    echo $num1+$num2;
+}elseif ($op=="-") {
+    echo $num1-$num2;
 }
-q("efewfew");
-function cube($ww){
-    return $ww*$ww*$ww;
+$grade=$_POST["grade"];
+switch($grade){
+    case "q":
+        echo "ewfwef";
+        break;
+    case "w":
+        echo "dwfrfrf";
+        break;
+    default:
+        echo "fsegegee";
+        break;
 }
-echo cube(3.323);
 ?>
 
 </form>
