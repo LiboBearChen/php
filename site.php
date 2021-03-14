74a9e01721253e3b4a41fd4621e2f3f28c16e30d
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2021-03-13 23:01:07 -0500

    update

--- site.php
+++ site.php
@@ -13,13 +13,27 @@ class Book{
     function __construct(){
         echo "New book created <br>";
     }
+
+    function a(){
+        if(($this->pages)>100){
+            return "more";
+        }
+        return "less";
+    }
 }
 
 $book1=new Book; 
 $book1->title="ddqw";
 $book1->pages=3243;
-
+$book2=new Book;
+$book2->pages=33;
 echo $book1->pages;
+echo "<br>";
+echo $book2->pages;
+echo "<br>";
+echo $book1->a();
+echo "<br>";
+echo $book2->a();
 ?>
 
 </form>
